5a3a3a08a864e841eb0c514c37912e6b8bddeeac
larbadge <112470475+larbadge@users.noreply.github.com>
2023-01-04 16:52:00 +0200

    LJ3-337

--- src/main/java/com/mikhalov/Main.java
+++ src/main/java/com/mikhalov/Main.java
@@ -1,44 +1,25 @@
 package com.mikhalov;
 
 import com.mikhalov.model.Car;
+import com.mikhalov.model.PassengerCar;
 import com.mikhalov.repository.CarArrayRepository;
 import com.mikhalov.service.CarService;
 
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) {
         CarService carService = new CarService(new CarArrayRepository());
-        carService.create();
+        List<Car> cars = carService.carsFromXml("carlist.xml");
+        cars.forEach(System.out::println);
 
-        List<Car> cars = Arrays.asList(carService.getAll());
+        System.out.println();
 
-        carService.findManufacturerByPrice(cars, 15000);
-        System.out.println(carService.countSum(cars));
-        System.out.println(carService.mapToMap(cars));
-        System.out.println(carService.statistic(cars));
-        System.out.println(carService.priceCheck(cars, 12000));
-        CarService carService1 = new CarService(new CarArrayRepository());
-        carService.create();
-        List<Car> cars1 = Arrays.asList(carService1.getAll());
-        List<List<Car>> carsLists = List.of(cars, cars1);
-        System.out.println(carService.innerList(carsLists, 10000));
-
-        Car newCar = carService.createNewCar(Car.CarType.CAR);
-        Car car = carService.mapToObject(carService.carToMap(newCar));
-        System.out.println(newCar);
-        System.out.println(car);
-        System.out.println(car.equals(newCar));
-
-        List<Map<String, Object>> maps = carService.toListOfMap(cars);
-        List<Car> cars2 = carService.toListOfCars(maps);
-        System.out.println(cars2);
-
+        List<Car> cars1 = carService.carsFromJson("carlist.json");
+        cars1.forEach(System.out::println);
+        PassengerCar pc = (PassengerCar) cars1.get(1);
+        System.out.println(pc.getPassengerCount());
     }
 }
 
